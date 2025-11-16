794ff00bbb5ba1c6ae9affef5f172e20379a88ea
ali-mahmoud24 <alimahmoud.2524@gmail.com>
2025-11-16 09:41:34 +0200

    feat(transportation): create transportation model, validators, and routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,8 @@ import userRoute from './userRoute.js';
 import authRoute from './authRoute.js';
 import hotelRoute from './hotelRoute.js';
 import roomTypeRoute from './roomTypeRoute.js';
+import transportationRoute from './transportationRoute.js';
+
 // Mount Routes
 const mountRoutes = (app) => {
   app.get('/', (req, res) => {
@@ -21,6 +23,7 @@ const mountRoutes = (app) => {
   app.use('/api/v1/auth', authRoute);
   app.use('/api/v1/hotels', hotelRoute);
   app.use('/api/v1/room-types', roomTypeRoute);
+  app.use('/api/v1/transportations', transportationRoute);
 };
 
 export default mountRoutes;
